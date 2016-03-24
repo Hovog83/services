f01b272f1e868a57657f7a037b4185ada7909c8a
VarduhiTsar <varduktsarukyan@yahoo.com>
2016-03-23 19:59:02 -0700

    app layout

--- config/app.php
+++ config/app.php
@@ -149,7 +149,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         // 'Illuminate\Html\HtmlServiceProvider',
-        Collective\Html\HtmlServiceProvider::class,
+      //  Collective\Html\HtmlServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -204,8 +204,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
+      //  'Form' => Collective\Html\FormFacade::class,
+      //  'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
