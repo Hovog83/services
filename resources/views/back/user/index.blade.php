5e840adead16914e69d78946d4e80839f6ed95d8
Hovo <Hovog83@gmail.com>
2016-03-25 12:31:48 +0400

    3/25

--- resources/views/back/user/index.blade.php
+++ resources/views/back/user/index.blade.php
@@ -1,6 +1,6 @@
 @extends('back.layout.main')
 @section('breadcrumb_first')
-    Categorie
+    User
 @stop
 @section('breadcrumb_second')
     List
@@ -15,7 +15,7 @@
             <!-- NEW WIDGET START -->
             <article class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <!-- Widget ID (each widget will need unique ID)-->
-                <div class="jarviswidget jarviswidget-color-blueDark" id="wid-id-2" data-widget-editbutton="true">
+                <div class="jarviswidget jarviswidget-color-blueDark" id="wid-id-7" data-widget-editbutton="true">
                     <!-- widget options:
                     usage: <div class="jarviswidget" id="wid-id-0" data-widget-editbutton="true">
 
@@ -49,15 +49,18 @@
                         <!-- widget content -->
                         <div class="widget-body no-padding">
                         <div class="col-xs-12">
-                            <a href="/admin/categorie/create" class="btn btn-primary" >Create</a>
+                            <a href="/admin/user/create" class="btn btn-primary" >Create</a>
                         </div>
                             <table id="datatable_cat" class="table table-striped table-bordered table-hover">
                                 <thead>
                                     <tr>
-                                        <th>Name</th>
+                                        <th>First Name</th>
+                                        <th>first Name</th>
+                                        <th>E-mail</th>
+                                        <th>Role</th>
                                         <th>Status</th>
-                                        <th>Icone</th>
-                                        <th>Order</th>
+                                        <th>Home Phone</th>
+                                        <th>Mobile Phone</th>
                                         <th>Actions</th>
                                     </tr>
                                 </thead>
@@ -93,25 +96,42 @@
         $(document).ready(function () {
 
             pageSetUp();
-
-            
             $('#datatable_cat').dataTable({
                 processing: true,
                 serverSide: true,
-                ajax: 'http://service.dev/admin/categorie/anyData',
-                "sDom": "<'dt-toolbar'<'col-xs-12 col-sm-6'f><'col-sm-6 col-xs-6 hidden-xs' l C>r>" +
+                ajax: 'http://service.dev/admin/user/anyData',
+                "sDom": "<'dt-toolbar'<'col-xs-12 col-sm-6'f ><'col-sm-3 col-xs-3 hidden-xs'   C><'col-sm-3 col-xs-3 hidden-xs'   l> r >" +
                 "t" +
                 "<'dt-toolbar-footer'<'col-sm-6 col-xs-12 hidden-xs'i><'col-sm-6 col-xs-12'p>>",
                 "autoWidth": true,
                 columns: [
-                    { data: 'name', name: 'name' },
+                    { data: 'firstname', name: 'firstname' },
+                    { data: 'lastname', name: 'lastname' },
+                    { data: 'email', name: 'email' },
+                    { data: 'role', name: 'role' },
                     { data: 'status', name: 'status' },
-                    { data: 'icone', name: 'icone' },
-                    { data: 'order', name: 'order' },
+                    { data: 'home_phone', name: 'home_phone' },
+                    { data: 'mobile_phone', name: 'mobile_phone' },
                     {data: 'action', name: 'action', orderable: false, searchable: false}
                 ]
             });
-           /* END COLUMN SHOW - HIDE */
+            /* // DOM Position key index //
+
+             l - Length changing (dropdown)
+             f - Filtering input (search)
+             t - The Table! (datatable)
+             i - Information (records)
+             p - Pagination (paging)
+             r - pRocessing
+             < and > - div elements
+             <"#id" and > - div with an id
+             <"class" and > - div with a class
+             <"#id.class" and > - div with an id and class
+
+             Also see: http://legacy.datatables.net/usage/features
+             */
+
+            /* END COLUMN SHOW - HIDE */
         })
     </script>
 @stop
